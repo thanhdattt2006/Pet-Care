1edd2a37a7025406c5cc7eeed87df8b8778b1b24
hongthai <thai2bake20157@gmail.com>
2025-09-11 19:32:07 +0700

    setup routing

--- anipat/src/routes/Routes.jsx
+++ anipat/src/routes/Routes.jsx
@@ -3,7 +3,19 @@ import Home from "../pages/Home";
 
 const createBrowser = () => {
     return <>
-        <Route path="/" element={ <Home/>} />
+        {/* người 1:Thại */}
+        <Route path="/" element={<Home />} />
+        
+        {/* người 2:Toại */}
+
+
+        {/* người 3:Đạt */}
+
+
+        {/* người 4:Cường */}
+
+        
+        {/* người 5:Huy */}
     </>
    
 }
