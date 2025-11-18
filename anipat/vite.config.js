36cd38c628e46cbe262e6218bde767a03fd158eb
thanhdattt2006 <thanhdattt2006@gmail.com>
2025-11-18 22:47:51 +0700

    deploy

--- anipat/vite.config.js
+++ anipat/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// vite.config.js
 
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// Thay YOUR_REPO_NAME bằng tên repository của bạn trên GitHub
+const repoName = 'Pet-Care';
+
 export default defineConfig({
   plugins: [react()],
-})
+  // ⬅️ THÊM DÒNG NÀY
+  base: `/${repoName}/`,
+  // base: '/tên-repo-của-bạn/', // Cú pháp đơn giản hơn
+});
